1871a9d66ac05de1f68f194723bc1f453b81051c
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-04-04 13:57:21 -0400

    add function to close Connection for service from menu and fixed service tests to work with new way to connect to database"

--- test/com/lms/service/test/AdministratorServiceTest.java
+++ test/com/lms/service/test/AdministratorServiceTest.java
@@ -4,16 +4,12 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
 import java.io.IOException;
-import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.AfterEach;
@@ -23,18 +19,15 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import com.lms.customExceptions.CriticalSQLException;
+import com.lms.customExceptions.DeleteException;
+import com.lms.customExceptions.InsertException;
 import com.lms.customExceptions.UpdateException;
-import com.lms.dao.AuthorDaoImpl;
-import com.lms.dao.BookDaoImpl;
-import com.lms.dao.BookLoansDaoImpl;
-import com.lms.dao.BorrowerDaoImpl;
-import com.lms.dao.LibraryBranchDaoImpl;
-import com.lms.dao.PublisherDaoImpl;
 import com.lms.model.Book;
 import com.lms.model.Borrower;
 import com.lms.model.Branch;
 import com.lms.model.Loan;
 import com.lms.service.AdministratorServiceImpl;
+import com.lms.service.BorrowerServiceImpl;
 
 public class AdministratorServiceTest {
 
@@ -46,73 +39,44 @@ public class AdministratorServiceTest {
 	private String borrowerName = "The Borrower Name";
 	private String borrowerAddress = "650 New Jersey Ave, Washington, DC 20001";
 	private String borrowerPhone = "1234567890";
-	
-	private static final LocalDate officialDueDate = LocalDate.now().plusWeeks(2);
-	
-	private static Connection conn = null;
-	private static BufferedReader br;
 
-	private static BookDaoImpl bookDaoImpl;
-	private static AuthorDaoImpl authorDaoImpl;
-	private static PublisherDaoImpl publisherDaoImpl;
-	private static LibraryBranchDaoImpl branchDaoImpl;
-	private static BorrowerDaoImpl borrowerDaoImpl;
-	private static BookLoansDaoImpl loanDaoImpl;
+	// due date is two weeks from now
+	private static final LocalDate officialDueDate = LocalDate.now().plusWeeks(2);
 	
 	private Borrower testBorrower;
 	private Book testBook;
 	private Loan testLoan;
 	private Branch testBranch;
 	
-	private AdministratorServiceImpl adminService;
+	private static AdministratorServiceImpl adminService;
+	private static BorrowerServiceImpl borrowerService;
 
 	@BeforeAll
-	public static void initAll() throws IOException, SQLException {
-		br = new BufferedReader(new FileReader(".config"));
-		List<String> authentication = new ArrayList<>();
-		String nextLine = "";
-		while((nextLine = br.readLine()) != null) {
-			authentication.add(nextLine);
-		}
-		br.close();
-
-		conn = (Connection) DriverManager.getConnection(
-				"jdbc:mysql://localhost:3306/libraryTest?useSSL=false&serverTimezone=UTC",
-				authentication.get(0), authentication.get(1));
-		
-		// simulate the same as the menu
-		conn.setAutoCommit(false);
-		
-		borrowerDaoImpl = new BorrowerDaoImpl(conn);
-		loanDaoImpl = new BookLoansDaoImpl(conn);
-		bookDaoImpl = new BookDaoImpl(conn);
-		branchDaoImpl = new LibraryBranchDaoImpl(conn);
-		authorDaoImpl = new AuthorDaoImpl(conn);
-		publisherDaoImpl = new PublisherDaoImpl(conn);
+	public static void initAll() throws IOException, SQLException, CriticalSQLException {
+		adminService = new AdministratorServiceImpl("test");
+		borrowerService = new BorrowerServiceImpl("test");
 	}
 	
 	@AfterAll
-	public static void cleanUp() throws IOException, SQLException {
-		conn.close();
+	public static void cleanUp() throws IOException, SQLException, CriticalSQLException {
+		adminService.closeConnection();
+		borrowerService.closeConnection();
 	}
 	
 	@BeforeEach
-	public void init() throws SQLException, CriticalSQLException {
-		testBorrower = borrowerDaoImpl.create(borrowerName, borrowerAddress, borrowerPhone);
-		testBook = bookDaoImpl.create(title, null, null);
-		testBranch = branchDaoImpl.create(branchName, branchAddress);
-		// due date is two weeks from now
-		testLoan = loanDaoImpl.create(testBook, testBorrower, testBranch, LocalDateTime.now(), officialDueDate);
-		adminService = new AdministratorServiceImpl("test");
+	public void init() throws SQLException, CriticalSQLException, InsertException {
+		testBorrower = adminService.createBorrower(borrowerName, borrowerAddress, borrowerPhone);
+		testBook = adminService.createBook(title, null, null);
+		testBranch = adminService.createBranch(branchName, branchAddress);
+		testLoan = borrowerService.borrowBook(testBorrower, testBook, testBranch, LocalDateTime.now(), officialDueDate);
 	}
 	
 	@AfterEach
-	public void tearThis() throws SQLException {
-		// WARNING maybe something that doesn't call the method we are trying to test
-		borrowerDaoImpl.delete(testBorrower);
-		bookDaoImpl.delete(testBook);
-		branchDaoImpl.delete(testBranch);
-		loanDaoImpl.delete(testLoan);
+	public void tearThis() throws SQLException, DeleteException {
+		adminService.deleteBorrower(testBorrower);
+		adminService.deleteBook(testBook);
+		adminService.deleteBranch(testBranch);
+		borrowerService.returnBook(testBorrower, testBook, testBranch, LocalDate.now());
 	}
 	
 	@DisplayName("Override due date correctly")
@@ -120,7 +84,13 @@ public class AdministratorServiceTest {
 	public void overrideDueDateForLoanTest() throws SQLException, UpdateException {
 		boolean success = adminService.overrideDueDateForLoan(testBook, testBorrower, testBranch,
 				officialDueDate.plusWeeks(1));
-		Loan foundLoan = loanDaoImpl.get(testBook, testBorrower, testBranch);
+		
+		List<Loan> listOfLoansWithOneLoan = getListThatMatches(testBook, testBorrower, testBranch);
+		
+		// make sure that there is only one loan, which there should be!
+		assertEquals(1, listOfLoansWithOneLoan.size());
+		
+		Loan foundLoan = listOfLoansWithOneLoan.get(0);
 
 		assertTrue(success);
 		assertEquals(testLoan.getDueDate().plusWeeks(1), foundLoan.getDueDate());
@@ -132,9 +102,29 @@ public class AdministratorServiceTest {
 		Book nonExistingBook = new Book(Integer.MAX_VALUE, "Some Title", null, null);
 		boolean success = adminService.overrideDueDateForLoan(nonExistingBook, testBorrower, testBranch,
 				officialDueDate.plusWeeks(1));
-		Loan foundLoan = loanDaoImpl.get(testBook, testBorrower, testBranch);
+		
+		List<Loan> listOfLoansWithNoLoan = getListThatMatches(nonExistingBook, testBorrower, testBranch);
+		
+		// The loan does not exist, so there should not be anything in it
+		assertEquals(0, listOfLoansWithNoLoan.size());
+		
+		
+		List<Loan> listOfLoansWithOneLoan = getListThatMatches(testBook, testBorrower, testBranch);
+		
+		// make sure that there is only one loan, which there should be!
+		assertEquals(1, listOfLoansWithOneLoan.size());
+		
+		Loan foundLoan = listOfLoansWithOneLoan.get(0);
 
 		assertFalse(success);
 		assertEquals(testLoan.getDueDate(), foundLoan.getDueDate());
+	}
+	
+	private List<Loan> getListThatMatches(Book book, Borrower borrower, Branch branch) {
+		List<Loan> tempListOfAllLoans = adminService.getAllLoans();
+		List<Loan> listOfAllLoansThatMatch = tempListOfAllLoans.parallelStream().filter(l -> l.getBook().equals(book) &&
+				l.getBorrower().equals(borrower) && l.getBranch().equals(branch))
+			.collect(Collectors.toList());
+		return listOfAllLoansThatMatch;
 	}
 }
