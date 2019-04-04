6a86a7a04c39865fe9e824a4665e66b50ff3f479
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-04-04 16:51:59 -0400

    add util class to help connect to database and ignoring properties instead of .config

---
+++ src/com/lms/service/util/ConnectingToDataBase.java
@@ -0,0 +1,64 @@
+package com.lms.service.util;
+
+import java.io.FileInputStream;
+import java.io.IOException;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import java.util.Properties;
+
+public final class ConnectingToDataBase {
+	final static Logger LOGGER = Logger.getLogger(ConnectingToDataBase.class.getName());
+
+	public static Connection connectingToDataBase(String env) throws SQLException {
+		Connection conn = null;
+		String user = "";
+		String password = "";
+		String url = "";
+
+		try (FileInputStream file = new FileInputStream(".properties")) {
+			
+			Properties prop = new Properties();
+			prop.load(file);
+			
+			user = prop.getProperty("user");
+			password = prop.getProperty("password");
+
+			if(env.equals("test")) {
+				url = prop.getProperty("testUrl");
+			} else if(env.equals("production")) {
+				url = prop.getProperty("url");
+			} else {
+				url = prop.getProperty("url");
+			}
+			
+
+		} catch (IOException e) {
+			LOGGER.log(Level.SEVERE, "I/O Error cannot read .property file: " + e.getMessage());
+		}
+		
+		try {
+			conn = (Connection) DriverManager.getConnection(url, user, password);
+			conn.setAutoCommit(false);
+			return conn;
+		} catch (SQLException ex) {
+		    // handle any errors
+			LOGGER.log(Level.WARNING, "SQLException: " + ex.getMessage() + " WITH SQLState: " +
+					ex.getSQLState() + " WITH VendorError: " + ex.getErrorCode());
+			throw new SQLException();
+		}
+	}
+	
+	public static void closingConnection(Connection conn) {
+		try {
+			conn.close();
+		} catch (SQLException e) {
+			LOGGER.log(Level.WARNING, "WARNING: Unable to close connection to database");
+		}
+	}
+}
