8d0eb7669a489cd0aea1c63b87df03e24aef41a4
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-04-05 15:52:01 -0400

    closed all unsued resources

--- src/com/lms/dao/BookDaoImpl.java
+++ src/com/lms/dao/BookDaoImpl.java
@@ -8,6 +8,7 @@ import java.sql.Types;
 import java.util.ArrayList;
 import java.util.List;
 
+import com.lms.dao.util.CloseResources;
 import com.lms.model.Author;
 import com.lms.model.Book;
 import com.lms.model.Publisher;
@@ -23,125 +24,161 @@ public class BookDaoImpl {
 	public Book create(String title, Author author, Publisher publisher) throws SQLException {
 		PreparedStatement prepareStatement = null;
 		ResultSet resultSet = null;
-		String sql = "INSERT INTO " + table + " (title, authId, pubId) VALUES (?, ?, ?);";
-		prepareStatement = conn.prepareStatement(sql);
-		prepareStatement.setString(1, title);
-		
-		if(author == null) {
-			prepareStatement.setNull(2, Types.NULL);
-		} else {
-			prepareStatement.setInt(2, author.getId());
-		}
-		
-		if(publisher == null) {
-			prepareStatement.setNull(3, Types.NULL);
-		} else {
-			prepareStatement.setInt(3, publisher.getId());
-		}
-		
-		prepareStatement.executeUpdate();
-		
-		sql = "SELECT * FROM " + table + " ORDER BY bookId DESC LIMIT 1;";
-		prepareStatement = conn.prepareStatement(sql);
-		resultSet = prepareStatement.executeQuery();
+		try {
+			String sql = "INSERT INTO " + table + " (title, authId, pubId) VALUES (?, ?, ?);";
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setString(1, title);
+			
+			if(author == null) {
+				prepareStatement.setNull(2, Types.NULL);
+			} else {
+				prepareStatement.setInt(2, author.getId());
+			}
+			
+			if(publisher == null) {
+				prepareStatement.setNull(3, Types.NULL);
+			} else {
+				prepareStatement.setInt(3, publisher.getId());
+			}
+			
+			prepareStatement.executeUpdate();
+			
+			// close first one
+			prepareStatement.close();
 
-		resultSet.next();
-		Book returnBook = new Book(resultSet.getInt("bookId"), resultSet.getString("title"), author, publisher);
-		return returnBook;
+			sql = "SELECT * FROM " + table + " ORDER BY bookId DESC LIMIT 1;";
+			prepareStatement = conn.prepareStatement(sql);
+			resultSet = prepareStatement.executeQuery();
+	
+			resultSet.next();
+			Book returnBook = new Book(resultSet.getInt("bookId"), resultSet.getString("title"), author, publisher);
+			return returnBook;
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closeResultSet(resultSet);
+			CloseResources.closePreparedStatement(prepareStatement);
+		}
 	}
 	
 	// if address or phone is null, then it will fill the query with sql null
 	public void update(Book book) throws SQLException {
 		PreparedStatement prepareStatement = null;
-		String sql = "UPDATE " + table + " SET title = ?, authId = ?, pubId = ?  WHERE bookId = ?;";
-		prepareStatement = conn.prepareStatement(sql);
-		prepareStatement.setString(1, book.getTitle());
-		
-		if(book.getAuthor() == null) {
-			prepareStatement.setNull(2, Types.NULL);
-		} else {
-			prepareStatement.setInt(2, book.getAuthor().getId());
+		try {
+			String sql = "UPDATE " + table + " SET title = ?, authId = ?, pubId = ?  WHERE bookId = ?;";
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setString(1, book.getTitle());
+			
+			if(book.getAuthor() == null) {
+				prepareStatement.setNull(2, Types.NULL);
+			} else {
+				prepareStatement.setInt(2, book.getAuthor().getId());
+			}
+			
+			if(book.getPublisher() == null) {
+				prepareStatement.setNull(3, Types.NULL);
+			} else {
+				prepareStatement.setInt(3, book.getPublisher().getId());
+			}
+			prepareStatement.setInt(4, book.getId());
+			prepareStatement.executeUpdate();
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closePreparedStatement(prepareStatement);
 		}
-		
-		if(book.getPublisher() == null) {
-			prepareStatement.setNull(3, Types.NULL);
-		} else {
-			prepareStatement.setInt(3, book.getPublisher().getId());
-		}
-		prepareStatement.setInt(4, book.getId());
-		prepareStatement.executeUpdate();
 	}
 	
 	public void delete(Book book) throws SQLException {
 		PreparedStatement prepareStatement = null;
-		String sql = "DELETE FROM " + table + " WHERE bookId = ?;";
-		prepareStatement = conn.prepareStatement(sql);
-		prepareStatement.setInt(1, book.getId());
-		prepareStatement.executeUpdate();
+		try {
+			String sql = "DELETE FROM " + table + " WHERE bookId = ?;";
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setInt(1, book.getId());
+			prepareStatement.executeUpdate();
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closePreparedStatement(prepareStatement);
+		}
 	}
 
 	public Book get(int id) throws SQLException {
 		PreparedStatement prepareStatement = null;
 		ResultSet resultSet = null;
-		String sql = "SELECT * FROM " + table + " WHERE bookId = ?;";
-		prepareStatement = conn.prepareStatement(sql);
-		prepareStatement.setInt(1, id);
-		resultSet = prepareStatement.executeQuery();
-		
-		Book returnBook = null;
-		if(resultSet.next()) {
-
-			Author author = null;
-			// mySQL will return 0 if column is mySQL null
-			if(resultSet.getInt("authId") != 0) {
-				AuthorDaoImpl authorDaoImpl = new AuthorDaoImpl(conn);
-				int authorId = resultSet.getInt("authId");
-				author = authorDaoImpl.get(authorId);
-			}
+		try {
+			String sql = "SELECT * FROM " + table + " WHERE bookId = ?;";
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setInt(1, id);
+			resultSet = prepareStatement.executeQuery();
 			
-			Publisher publisher = null;
-			// mySQL will return 0 if column is mySQL null
-			if(resultSet.getInt("pubId") != 0) {
-				PublisherDaoImpl publisherDaoImpl = new PublisherDaoImpl(conn);
-				int publisherId = resultSet.getInt("pubId");
-				publisher = publisherDaoImpl.get(publisherId);
+			Book returnBook = null;
+			if(resultSet.next()) {
+	
+				Author author = null;
+				// mySQL will return 0 if column is mySQL null
+				if(resultSet.getInt("authId") != 0) {
+					AuthorDaoImpl authorDaoImpl = new AuthorDaoImpl(conn);
+					int authorId = resultSet.getInt("authId");
+					author = authorDaoImpl.get(authorId);
+				}
+				
+				Publisher publisher = null;
+				// mySQL will return 0 if column is mySQL null
+				if(resultSet.getInt("pubId") != 0) {
+					PublisherDaoImpl publisherDaoImpl = new PublisherDaoImpl(conn);
+					int publisherId = resultSet.getInt("pubId");
+					publisher = publisherDaoImpl.get(publisherId);
+				}
+				
+				returnBook = new Book(resultSet.getInt("bookId"), resultSet.getString("title"),
+						author, publisher);
 			}
-			
-			returnBook = new Book(resultSet.getInt("bookId"), resultSet.getString("title"),
-					author, publisher);
+			return returnBook;
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closeResultSet(resultSet);
+			CloseResources.closePreparedStatement(prepareStatement);
 		}
-		return returnBook;
 	}
 
 	public List<Book> getAll() throws SQLException {
 		PreparedStatement prepareStatement = null;
 		ResultSet resultSet = null;
-		List<Book> returnList = new ArrayList<>();
-		
-		String sql = "SELECT * FROM " + table + ";";
-		prepareStatement = conn.prepareStatement(sql);
-		resultSet = prepareStatement.executeQuery();
-		while (resultSet.next()) {
-
-			Author author = null;
-			// mySQL will return 0 if column is mySQL null
-			if(resultSet.getInt("authId") != 0) {
-				AuthorDaoImpl authorDaoImpl = new AuthorDaoImpl(conn);
-				int authorId = resultSet.getInt("authId");
-				author = authorDaoImpl.get(authorId);
-			}
+		try {
+			List<Book> returnList = new ArrayList<>();
 			
-			Publisher publisher = null;
-			// mySQL will return 0 if column is mySQL null
-			if(resultSet.getInt("pubId") != 0) {
-				PublisherDaoImpl publisherDaoImpl = new PublisherDaoImpl(conn);
-				int publisherId = resultSet.getInt("pubId");
-				publisher = publisherDaoImpl.get(publisherId);
+			String sql = "SELECT * FROM " + table + ";";
+			prepareStatement = conn.prepareStatement(sql);
+			resultSet = prepareStatement.executeQuery();
+			while (resultSet.next()) {
+	
+				Author author = null;
+				// mySQL will return 0 if column is mySQL null
+				if(resultSet.getInt("authId") != 0) {
+					AuthorDaoImpl authorDaoImpl = new AuthorDaoImpl(conn);
+					int authorId = resultSet.getInt("authId");
+					author = authorDaoImpl.get(authorId);
+				}
+				
+				Publisher publisher = null;
+				// mySQL will return 0 if column is mySQL null
+				if(resultSet.getInt("pubId") != 0) {
+					PublisherDaoImpl publisherDaoImpl = new PublisherDaoImpl(conn);
+					int publisherId = resultSet.getInt("pubId");
+					publisher = publisherDaoImpl.get(publisherId);
+				}
+				
+				returnList.add(new Book(resultSet.getInt("bookId"), resultSet.getString("title"),
+						author, publisher));
 			}
-			
-			returnList.add(new Book(resultSet.getInt("bookId"), resultSet.getString("title"),
-					author, publisher));
+			return returnList;
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closeResultSet(resultSet);
+			CloseResources.closePreparedStatement(prepareStatement);
 		}
-		return returnList;
 	}
 }
--- src/com/lms/dao/PublisherDaoImpl.java
+++ src/com/lms/dao/PublisherDaoImpl.java
@@ -5,6 +5,9 @@ import java.sql.SQLException;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.Types;
+
+import com.lms.dao.util.CloseResources;
+
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,92 +25,128 @@ public class PublisherDaoImpl {
 	public Publisher create(String publisherName, String publisherAddress, String publisherPhone) throws SQLException {
 		PreparedStatement prepareStatement = null;
 		ResultSet resultSet = null;
-		String sql = "INSERT INTO " + table + " (publisherName, publisherAddress, publisherPhone) VALUES (?, ?, ?);";
-		prepareStatement = conn.prepareStatement(sql);
-		prepareStatement.setString(1, publisherName);
-
-		if(publisherAddress == null) {
-			prepareStatement.setNull(2, Types.NULL);
-		} else {
-			prepareStatement.setString(2, publisherAddress);
+		try {
+			String sql = "INSERT INTO " + table + " (publisherName, publisherAddress, publisherPhone) VALUES (?, ?, ?);";
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setString(1, publisherName);
+	
+			if(publisherAddress == null) {
+				prepareStatement.setNull(2, Types.NULL);
+			} else {
+				prepareStatement.setString(2, publisherAddress);
+			}
+	
+			if(publisherPhone == null) {
+				prepareStatement.setNull(3, Types.NULL);
+			} else {
+				prepareStatement.setString(3, publisherPhone);
+			}
+	
+			prepareStatement.executeUpdate();
+			
+			// close previous one
+			prepareStatement.close();
+			
+			sql = "SELECT * FROM " + table + " ORDER BY publisherId DESC LIMIT 1;";
+			prepareStatement = conn.prepareStatement(sql);
+			resultSet = prepareStatement.executeQuery();
+	
+			resultSet.next();
+			Publisher returnPublisher = new Publisher(resultSet.getInt("publisherId"), resultSet.getString("publisherName"),
+					resultSet.getString("publisherAddress"), resultSet.getString("publisherPhone"));
+			return returnPublisher;
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closeResultSet(resultSet);
+			CloseResources.closePreparedStatement(prepareStatement);
 		}
-
-		if(publisherPhone == null) {
-			prepareStatement.setNull(3, Types.NULL);
-		} else {
-			prepareStatement.setString(3, publisherPhone);
-		}
-
-		prepareStatement.executeUpdate();
-		
-		sql = "SELECT * FROM " + table + " ORDER BY publisherId DESC LIMIT 1;";
-		prepareStatement = conn.prepareStatement(sql);
-		resultSet = prepareStatement.executeQuery();
-
-		resultSet.next();
-		Publisher returnPublisher = new Publisher(resultSet.getInt("publisherId"), resultSet.getString("publisherName"),
-				resultSet.getString("publisherAddress"), resultSet.getString("publisherPhone"));
-		return returnPublisher;
 	}
 	
 	// if address or phone is null, then it will fill the query with sql null
 	public void update(Publisher publisher) throws SQLException {
 		PreparedStatement prepareStatement = null;
-		String sql = "UPDATE " + table + " SET publisherName = ?, publisherAddress = ?, publisherPhone = ?  WHERE publisherId = ?;";
-		prepareStatement = conn.prepareStatement(sql);
-		prepareStatement.setString(1, publisher.getName());
-		
-		if(publisher.getAddress() == null) {
-			prepareStatement.setNull(2, Types.NULL);
-		} else {
-			prepareStatement.setString(2, publisher.getAddress());
+		try {
+			String sql = "UPDATE " + table + " SET publisherName = ?, publisherAddress = ?, publisherPhone = ?  WHERE publisherId = ?;";
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setString(1, publisher.getName());
+			
+			if(publisher.getAddress() == null) {
+				prepareStatement.setNull(2, Types.NULL);
+			} else {
+				prepareStatement.setString(2, publisher.getAddress());
+			}
+			
+			if(publisher.getPhone() == null) {
+				prepareStatement.setNull(3, Types.NULL);
+			} else {
+				prepareStatement.setString(3, publisher.getPhone());
+			}
+			prepareStatement.setInt(4, publisher.getId());
+			prepareStatement.executeUpdate();
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closePreparedStatement(prepareStatement);
 		}
-		
-		if(publisher.getPhone() == null) {
-			prepareStatement.setNull(3, Types.NULL);
-		} else {
-			prepareStatement.setString(3, publisher.getPhone());
-		}
-		prepareStatement.setInt(4, publisher.getId());
-		prepareStatement.executeUpdate();
 	}
 	
 	public void delete(Publisher publisher) throws SQLException {
 		PreparedStatement prepareStatement = null;
-		String sql = "DELETE FROM " + table + " WHERE publisherId = ?;";
-		prepareStatement = conn.prepareStatement(sql);
-		prepareStatement.setInt(1, publisher.getId());
-		prepareStatement.executeUpdate();
+		try {
+			String sql = "DELETE FROM " + table + " WHERE publisherId = ?;";
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setInt(1, publisher.getId());
+			prepareStatement.executeUpdate();
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closePreparedStatement(prepareStatement);
+		}
 	}
 
 	public Publisher get(int id) throws SQLException {
 		PreparedStatement prepareStatement = null;
 		ResultSet resultSet = null;
-		String sql = "SELECT * FROM " + table + " WHERE publisherId = ?;";
-		prepareStatement = conn.prepareStatement(sql);
-		prepareStatement.setInt(1, id);
-		resultSet = prepareStatement.executeQuery();
-		
-		Publisher returnPublisher = null;
-		if(resultSet.next()) {
-			returnPublisher = new Publisher(resultSet.getInt("publisherId"), resultSet.getString("publisherName"),
-					resultSet.getString("publisherAddress"), resultSet.getString("publisherPhone"));
+		try {
+			String sql = "SELECT * FROM " + table + " WHERE publisherId = ?;";
+			prepareStatement = conn.prepareStatement(sql);
+			prepareStatement.setInt(1, id);
+			resultSet = prepareStatement.executeQuery();
+			
+			Publisher returnPublisher = null;
+			if(resultSet.next()) {
+				returnPublisher = new Publisher(resultSet.getInt("publisherId"), resultSet.getString("publisherName"),
+						resultSet.getString("publisherAddress"), resultSet.getString("publisherPhone"));
+			}
+			return returnPublisher;
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closeResultSet(resultSet);
+			CloseResources.closePreparedStatement(prepareStatement);
 		}
-		return returnPublisher;
 	}
 
 	public List<Publisher> getAll() throws SQLException {
 		PreparedStatement prepareStatement = null;
 		ResultSet resultSet = null;
-		List<Publisher> returnList = new ArrayList<>();
-		
-		String sql = "SELECT * FROM " + table + ";";
-		prepareStatement = conn.prepareStatement(sql);
-		resultSet = prepareStatement.executeQuery();
-		while (resultSet.next()) {
-			returnList.add(new Publisher(resultSet.getInt("publisherId"), resultSet.getString("publisherName"),
-					resultSet.getString("publisherAddress"), resultSet.getString("publisherPhone")));
+		try {
+			List<Publisher> returnList = new ArrayList<>();
+			
+			String sql = "SELECT * FROM " + table + ";";
+			prepareStatement = conn.prepareStatement(sql);
+			resultSet = prepareStatement.executeQuery();
+			while (resultSet.next()) {
+				returnList.add(new Publisher(resultSet.getInt("publisherId"), resultSet.getString("publisherName"),
+						resultSet.getString("publisherAddress"), resultSet.getString("publisherPhone")));
+			}
+			return returnList;
+		} catch (SQLException e) {
+			throw new SQLException();
+		} finally {
+			CloseResources.closeResultSet(resultSet);
+			CloseResources.closePreparedStatement(prepareStatement);
 		}
-		return returnList;
 	}
 }
