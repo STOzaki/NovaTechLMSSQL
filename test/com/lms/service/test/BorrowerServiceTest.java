5e44dcbf55af6272730f925338d69150d3809dbd
Salem Ozaki <SOzaki19@cornellcollege.edu>
2019-04-11 10:38:57 -0400

    all assertions now import junit 5 not 4 for service

--- test/com/lms/service/test/BorrowerServiceTest.java
+++ test/com/lms/service/test/BorrowerServiceTest.java
@@ -1,8 +1,8 @@
 package com.lms.service.test;
 
-import static org.junit.Assert.assertFalse;
-import static org.junit.Assert.assertNull;
-import static org.junit.Assert.assertTrue;
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import java.io.IOException;
